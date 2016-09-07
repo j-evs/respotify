aa02b0df2d545d5946b8edab77b6a613e896aefa
Jan <jan.evseev@gmail.com>
2016-09-07 17:35:49 +0300

    Added designated components for tracks and albums, added track preview feature

--- src/components/AlbumList.jsx
+++ src/components/AlbumList.jsx
@@ -1,17 +1,42 @@
 import React from 'react';
+import Album from './Album';
 
 const AlbumList = (props) => {
-  const albums = props.albums.map((album) => <li>{album.name}</li>);
+  const albums = props.albums.map((album) => {
+    return (
+      <Album
+        key={album.id}
+        album={album}
+        getTracks={props.getTracks}
+      />
+    );
+  });
 
   return (
-    <ul>
-      {albums}
-    </ul>
+    <div className="col-md-4" style={AlbumList.styles.div}>
+      <ul style={AlbumList.styles.ul}>
+        {albums}
+      </ul>
+    </div>
   );
 };
 
 AlbumList.propTypes = {
   albums: React.PropTypes.array.isRequired,
+  getTracks: React.PropTypes.func.isRequired,
+};
+
+AlbumList.styles = {
+  div: {
+    width: 370,
+    marginLeft: 30,
+    textAlign: 'right',
+    maxHeight: '85vh',
+    overflowY: 'auto',
+  },
+  ul: {
+    listStyle: 'none',
+  },
 };
 
 export default AlbumList;
---
+++ src/components/TrackList.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Track from './Track';
+
+const TrackList = (props) => {
+  const tracks = props.tracks.map((track) => {
+    return (
+      <Track
+        key={track.id}
+        track={track}
+        playPreview={props.playPreview}
+      />
+    );
+  });
+
+  return (
+    <div className="col-md-3">
+      <ul style={{ listStyle: 'none' }}>
+        {tracks}
+      </ul>
+    </div>
+  );
+};
+
+TrackList.propTypes = {
+  tracks: React.PropTypes.array.isRequired,
+  playPreview: React.PropTypes.func.isRequired,
+};
+
+export default TrackList;
